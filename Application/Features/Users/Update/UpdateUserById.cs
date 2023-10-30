6dccd5eb7949e3d858663a6412cf45f95e57f178
Zhyrgalbek1 <137794850+Zhyrgalbek1@users.noreply.github.com>
2023-10-30 20:50:26 +0600

    Added Handler UpdateUserById

---
+++ Application/Features/Users/Update/UpdateUserById.cs
@@ -0,0 +1,61 @@
+﻿using Application.Shared;
+using Domain.Repositories;
+
+namespace Application.Features.Users.Update;
+
+public class UpdateUserByIdCommand : IRequest<Result<UpdateUserByIdDto>>
+{
+    public Guid Id { get; init; }
+    public string? Username { get; init; }
+    public string? Password { get; init; }
+    public string? Email { get; init; }
+}
+
+public class UpdateUserByIdValidator : AbstractValidator<UpdateUserByIdDto>
+{
+    public UpdateUserByIdValidator()
+    {
+        RuleFor(u => u.Username).NotEmpty().Length(2, 200);
+        RuleFor(p => p.Password).NotEmpty().Length(2, 200);
+        RuleFor(e => e.Email).EmailAddress().NotEmpty();
+    }
+}
+
+public class UpdateUserByIdCommandHandler : IRequestHandler<UpdateUserByIdCommand, Result<UpdateUserByIdDto>>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IUnitOfWork _uow;
+
+    public UpdateUserByIdCommandHandler(IUserRepository userRepository, IUnitOfWork uow)
+    {
+        _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+        _uow = uow ?? throw new NullReferenceException(nameof(uow));
+    }
+
+    public async Task<Result<UpdateUserByIdDto>> Handle(UpdateUserByIdCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetByIdAsync(request.Id);
+        if (user == null)
+            return Result.Fail("User is not found");
+        if (request.Username != null)
+            user.Username = request.Username;
+        if (request.Password != null)
+            user.Password = request.Password;
+        if (request.Email != null)
+            user.Email = request.Email;
+
+        _userRepository.Update(user);
+        _uow.SaveCommitAsync();
+
+        var response = new UpdateUserByIdDto
+        {
+            Email = user.Email,
+            Password = user.Password,
+            Id = user.Id,
+            Username = user.Username,
+            Role = user.Role,
+        };
+
+        return Result.Ok(response);
+    } 
+}
---
+++ Application/Features/Users/Update/UpdateUserByIdDto.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Enums;
+
+namespace Application.Features.Users.Update;
+
+public record UpdateUserByIdDto
+{
+    public Guid Id { get; init; }
+    public required string Username { get; init; }
+    public required string Password { get; init; }
+
+    public required string Email { get; init; }
+    public Role Role { get; init; }
+
+}
