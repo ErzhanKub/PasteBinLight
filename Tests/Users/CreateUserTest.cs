a3319794dd4a3d67a50e1e7fa1ea0d72e64bbcc9
Kanat Sadyrbekov <kanat890088@gmail.com>
2023-10-31 20:23:24 +0600

    CreateTest
    create test for create users

---
+++ Tests/Users/CreateUserTest.cs
@@ -0,0 +1,40 @@
+﻿using Application.Features.Users.Create;
+using Application.Shared;
+using Domain.Entities;
+using Domain.Repositories;
+
+namespace Tests.Users;
+
+public class CreateUserTest
+{
+    private readonly Mock<IUserRepository> _userRepositoryMock;
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+
+    public CreateUserTest()
+    {
+        _userRepositoryMock = new Mock<IUserRepository>();
+        _unitOfWorkMock = new Mock<IUnitOfWork>();
+    }
+
+    [Fact]
+    public async Task Handle_ValidCommand_ShouldCreateUser()
+    {
+        // Arrange
+        var command = new CreateUserCommand
+        {
+            Username = "Test",
+            Email = "test.user@example.com",
+            Password = "password123",
+        };
+
+        var handler = new CreateUserCommandHandler(_userRepositoryMock.Object, _unitOfWorkMock.Object);
+
+        // Act
+        var result = await handler.Handle(command, default);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        _userRepositoryMock.Verify(x => x.CreateAsync(It.IsAny<User>()), Times.Once);
+        _unitOfWorkMock.Verify(x => x.SaveCommitAsync(), Times.Once);
+    }
+}
