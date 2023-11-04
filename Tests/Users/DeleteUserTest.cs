5f7ce83ed402bb502ce5aae01ec85eeadb66273e
Kanat Sadyrbekov <kanat890088@gmail.com>
2023-11-04 18:37:14 +0600

    Add deleteTest and LoginTest

---
+++ Tests/Users/DeleteUserTest.cs
@@ -0,0 +1,43 @@
+﻿using Application.Features.Users.Delete;
+using Application.Shared;
+using Domain.Entities;
+using Domain.Repositories;
+
+namespace Tests.Users
+{
+    public class DeleteUserTest
+    {
+
+        private readonly Mock<IUserRepository> _userRepositoryMock;
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+
+        public DeleteUserTest()
+        {
+            _userRepositoryMock = new Mock<IUserRepository>();
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+        }
+
+        [Fact]
+        public async Task Handle_ValidCommand_ShouldDeleteUser()
+        {
+            // Arrange
+            var command = new DeleteUsersByIdsCommand
+            {
+                Id = new Guid[] { Guid.NewGuid() }
+            };
+            var handler = new DeleteUsersByIdsHandler(_userRepositoryMock.Object, _unitOfWorkMock.Object);
+
+            _userRepositoryMock.Setup(x => x.DeleteRangeAsync(It.IsAny<Guid[]>()))
+                .ReturnsAsync(command.Id);
+
+            // Act
+            var result = await handler.Handle(command, default);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Should().BeEquivalentTo(command.Id);
+            _userRepositoryMock.Verify(x => x.DeleteRangeAsync(It.IsAny<Guid[]>()), Times.Once);
+            _unitOfWorkMock.Verify(x => x.SaveCommitAsync(), Times.Once);
+        }
+    }
+}
---
+++ Tests/Users/LoginUserTest.cs
@@ -0,0 +1,45 @@
+﻿using Application.Features.Users.Login;
+using Domain.Entities;
+using Domain.Enums;
+using Domain.Repositories;
+using Microsoft.Extensions.Configuration;
+
+namespace Tests.Users
+{
+    public class LoginUserTest
+    {
+        private readonly Mock<IUserRepository> _userRepositoryMock;
+        private readonly Mock<IConfiguration> _configurationMock;
+
+        public LoginUserTest()
+        {
+            _userRepositoryMock = new Mock<IUserRepository>();
+            _configurationMock = new Mock<IConfiguration>();
+        }
+
+        [Fact]
+        public async Task Handle_ValidRequest_ShouldReturnToken()
+        {
+            // Arrange
+            var request = new LoginRequest
+            {
+                Username = "test.user@example.com",
+                Password = "password123",
+            };
+            var handler = new LoginHandler(_configurationMock.Object, _userRepositoryMock.Object);
+
+            _userRepositoryMock.Setup(x => x.CheckUserCredentialsAsync(request.Username, request.Password))
+                .ReturnsAsync(new User { Username = request.Username, Password = request.Password, Email = "qwerty" });
+            _configurationMock.Setup(x => x["Jwt"]).Returns("qwertyuiopasdfgh");
+
+
+            // Act
+            var result = await handler.Handle(request, default);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Should().NotBeNullOrEmpty();
+            _userRepositoryMock.Verify(x => x.CheckUserCredentialsAsync(It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+        }
+    }
+}
